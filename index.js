0b087ebbb87b26ad0bb8a10f3cbfda4974f95dca
sebastien <lorber.sebastien@gmail.com>
2025-01-24 11:46:04 +0100

    test await import too

--- index.js
+++ index.js
@@ -18,10 +18,20 @@ async function testWorker({esm}) {
     console.log("Worker success result",{esm,result});
 }
 
+async function testImport({esm}) {
+    const workerFile = esm ? 'worker.mjs' : 'worker.js';
+    const filename = path.resolve(__dirname, workerFile);
+    console.log('Import filename', {esm,filename});
+    const module = await import(filename)
+    console.log("Import success",{esm,module})
+}
+
 async function test() {
     await Promise.all([
         testWorker({esm: true}),
-        testWorker({esm: false})
+        testWorker({esm: false}),
+        testImport({esm: true}),
+        testImport({esm: false}),
     ])
 }
 
